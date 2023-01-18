8712dff3b106df9cc7784fa82e87f918006b4d81
RasulPirsoltanov1 <7m0uvuf@code.edu.az>
2023-01-19 01:46:26 +0400

    first commit

---
+++ WebUI/Program.cs
@@ -0,0 +1,15 @@
+using DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var constr = builder.Configuration["ConnectionStrings:Default"];
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(constr));
+var app = builder.Build();
+app.UseStaticFiles();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+
+app.Run();
